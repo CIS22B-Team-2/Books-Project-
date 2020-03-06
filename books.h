7aa78bf7253cf63abd793e71ff2ccadca96aca24
ZiliWang271 <61486412+ZiliWang271@users.noreply.github.com>
2020-03-05 19:40:43 -0800

    Update books.h

--- books.h
+++ books.h
@@ -1,10 +1,49 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+
 // Books object
-class Books
+class Book
 {
 private:
-  
+	int ISBN;
+	std::string title; 
+	std::string publisher;
+	int year; 
+	int month;
+	int day; 
+	int quantity; 
+	double wholesale; 
+	double retail; 
+	std::string author; 
 
+public: 
+	// Constructors
+	Book(); 
+	Book(int i, std::string t, std::string p, int y, int m, int d, int q, double w, double r, std::string a); // all params constructor
 
+	// getters
+	int getISBN();
+	std::string getTitle();
+	std::string getPublisher();
+	int getYear();
+	int getMonth();
+	int getDay();
+	int getQuantity();
+	double getWholesale();
+	double getRetail();
+	std::string getAuthor();
 
+	// setters 
+	void setISBN(int i);
+	void setTitle(std::string t);
+	void setPublisher(std::string p);
+	void setYear(int y);
+	void setMonth(int m);
+	void setDay(int d);
+	void setQuantity(int q);
+	void setWholesale(double w);
+	void setRetail(double r);
+	void setAuthor(std::string a);
 };
