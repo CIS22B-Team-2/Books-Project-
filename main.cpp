084060852b5ae3ce493fc47ca12185ac26a39eea
Stephen Klein <stephendklein08@gmail.com>
2020-03-13 09:26:03 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -12,3 +12,31 @@ int main()
   
   return 0;  
 }
+
+
+
+
+// Functions - Stephen
+//The function below sorts the books by quantity;
+// Largest quantity first;
+void sortBooksQuantity(Book arr[], int size) {
+  //sort the books by 
+  int minIndex;
+  int minValue;
+  
+  for (int start = 0; start < (size - 1); start++) {
+    minIndex = start;
+    minValue = arr[start].getQuantity();
+    for (int index = start + 1; index < size; index++) {
+      if(arr[index].getQuantity() > minValue) {
+        minValue = arr[index].getQuantity();
+        minIndex = index;
+      }
+    }
+    swap(arr[minIndex], arr[start]);
+  }
+
+  cout << "AFTER: " << endl;
+  listBooks(arr);
+
+};
